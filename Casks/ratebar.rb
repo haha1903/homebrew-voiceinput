e7204f3ae3520c816a1dd4983a0b18990e8f0f1e
haha1903 <haha1903@users.noreply.github.com>
2026-05-05 22:18:33 +1000

    ratebar: update repo URL to lowercase haha1903/ratebar

--- Casks/ratebar.rb
+++ Casks/ratebar.rb
@@ -2,10 +2,10 @@ cask "ratebar" do
   version "0.1.2"
   sha256 "af3d556839c69ce2d04ea6e78690eedf9c85e3d570446f7ec97feee46bae0625"
 
-  url "https://github.com/haha1903/RateBar/releases/download/v#{version}/RateBar-#{version}.zip"
+  url "https://github.com/haha1903/ratebar/releases/download/v#{version}/RateBar-#{version}.zip"
   name "RateBar"
   desc "macOS menu bar exchange-rate utility (AUD → CNY/USD/JPY/EUR)"
-  homepage "https://github.com/haha1903/RateBar"
+  homepage "https://github.com/haha1903/ratebar"
 
   livecheck do
     url :url
